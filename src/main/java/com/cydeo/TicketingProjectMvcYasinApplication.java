71c091adbd109c1759fef8c8e8c33261a79215dd
yasinTawasulli <royesh14@gmail.com>
2022-10-06 21:13:10 -0700

    part 1-3

---
+++ src/main/java/com/cydeo/TicketingProjectMvcYasinApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectMvcYasinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectMvcYasinApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cydeo/boostrap/DataGenerator.java
@@ -0,0 +1,13 @@
+package com.cydeo.bootstrap;
+
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.impl.RoleServiceImpl;
+
+public class DataGenerator {
+
+
+
+
+}
